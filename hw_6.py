56e9349c6bc55cc20f999130109b0b56292ce893
urmatbekovnaa <urmatbekovnaad@gmail.com>
2024-09-19 12:00:32 +0600

    hw_6

--- hw_6.py
+++ hw_6.py
@@ -1,3 +1,21 @@
+def bubble_sort(array):
+    n = len(array)
+    for i in range(n):
+        for j in range(0, n - i - 1):
+            if array[j] > array[j + 1]:
+                array[j], array[j + 1] = array[j + 1], array[j]
+    return array
+
+def selection_sort(array):
+    for i in range(len(array)):
+        min_idx = i
+        for j in range(i + 1, len(array)):
+            if array[min_idx] > array[j]:
+                min_idx = j
+        array[i], array[min_idx] = array[min_idx], array[i]
+    return array
+
+
 def binary_search(array, target):
     first = 0
     last = len(array) - 1
